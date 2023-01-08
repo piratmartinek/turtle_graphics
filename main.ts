9e54848a505a9d9b2f8025a29541afa6f18d0157
piratmartinek <121960967+piratmartinek@users.noreply.github.com>
2023-01-08 14:27:18 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+turtle.setPosition(2, 0)
+turtle.pen(TurtlePenMode.Down)
+turtle.turnRight()
+turtle.forward(2)
+turtle.turnRight()
+for (let index = 0; index <= 2; index++) {
+    turtle.forward(4)
+    turtle.turnRight()
+}
+turtle.forward(2)
